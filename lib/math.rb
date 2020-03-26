a470462e589f0fd92000d9ee6c27504a615d1471
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-26 14:49:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,16 +9,18 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  result = num1 /
-
+  result = num1 / num2
+  return result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  return result
 end
 
 def modulo(num1, num2)
-
+  result = num1 % num2
+  return result
 end
 
 def square_root(num)
